886f1116dc199039ea7849d816582cd551294037
Tomek Kopczynski <tomek@kopczynski.net.pl>
2017-12-06 20:18:44 +0100

    Introduced some caching

--- app/controllers/WeatherController.scala
+++ app/controllers/WeatherController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
+import play.api.cache.AsyncCacheApi
 import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc._
@@ -13,22 +14,28 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * Created by Tomasz Kopczynski.
   */
 @Singleton
-class WeatherController @Inject()(cc: ControllerComponents, ws: WSClient, config: Configuration) extends AbstractController(cc) {
+class WeatherController @Inject()(cc: ControllerComponents, ws: WSClient, config: Configuration, cache: AsyncCacheApi) extends AbstractController(cc) {
 
   def locateEndpoint: String = config.get[String]("api.weather.locate")
+
   def locationWeatherEndpoint: String = config.get[String]("api.weather.locationWeather")
+
   def logger: Logger = Logger(this.getClass.getSimpleName)
 
   def weather(city: String): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    ws.url(locateEndpoint).addQueryStringParameters("query" -> city).get()
-      .map {
-        response => response.json \ 0 \ "woeid"
-      }
-      .map(_.as[Int])
-      .map(woeid => String.valueOf(woeid))
-      .flatMap { woeid =>
-        ws.url(s"$locationWeatherEndpoint/$woeid").get()
-      }
+    val locationWoeid = cache.getOrElseUpdate[String](s"location.$city") {
+      logger.info("querying for location woeid")
+      ws.url(locateEndpoint).addQueryStringParameters("query" -> city).get()
+        .map {
+          response => response.json \ 0 \ "woeid"
+        }
+        .map(_.as[Int])
+        .map(String.valueOf)
+    }
+
+    locationWoeid.flatMap { woeid =>
+      ws.url(s"$locationWeatherEndpoint/$woeid").get()
+    }
       .map {
         response => response.json \ "consolidated_weather" \ 0 \ "the_temp"
       }
@@ -37,8 +44,8 @@ class WeatherController @Inject()(cc: ControllerComponents, ws: WSClient, config
       .recover {
         case e: Exception => {
           logger.error("Error when calling Weather API", e)
-          InternalServerError(Json.obj("error" -> e.getMessage))
+          ServiceUnavailable(Json.obj("error" -> e.getMessage))
         }
       }
-    }
+  }
 }
--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ scalaVersion := "2.12.3"
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
 libraryDependencies += ws
+libraryDependencies += ehcache
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "pl.net.kopczynski.controllers._"
