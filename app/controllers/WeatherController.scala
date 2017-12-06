6eb41fe18dbb3a668d4eebc925de2ade8ac700f7
Tomek Kopczynski <tomek@kopczynski.net.pl>
2017-12-06 13:42:20 +0100

    Basic error handling

--- app/controllers/WeatherController.scala
+++ app/controllers/WeatherController.scala
@@ -2,10 +2,10 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.Configuration
 import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc._
+import play.api.{Configuration, Logger}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -17,14 +17,14 @@ class WeatherController @Inject()(cc: ControllerComponents, ws: WSClient, config
 
   def locateEndpoint: String = config.get[String]("api.weather.locate")
   def locationWeatherEndpoint: String = config.get[String]("api.weather.locationWeather")
+  def logger: Logger = Logger(this.getClass.getSimpleName)
 
   def weather(city: String): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    ws.url(locateEndpoint).addQueryStringParameters("query" -> city)
-      .get()
+    ws.url(locateEndpoint).addQueryStringParameters("query" -> city).get()
       .map {
         response => response.json \ 0 \ "woeid"
       }
-      .map(jsLookupResult => jsLookupResult.as[Int])
+      .map(_.as[Int])
       .map(woeid => String.valueOf(woeid))
       .flatMap { woeid =>
         ws.url(s"$locationWeatherEndpoint/$woeid").get()
@@ -32,7 +32,13 @@ class WeatherController @Inject()(cc: ControllerComponents, ws: WSClient, config
       .map {
         response => response.json \ "consolidated_weather" \ 0 \ "the_temp"
       }
-      .map(jsLookupResult => jsLookupResult.as[Double])
+      .map(_.as[Double])
       .map(temperature => Ok(Json.obj("current_temperature" -> temperature)))
+      .recover {
+        case e: Exception => {
+          logger.error("Error when calling Weather API", e)
+          InternalServerError(Json.obj("error" -> e.getMessage))
+        }
+      }
     }
 }
